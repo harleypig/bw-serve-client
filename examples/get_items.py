0e52540c3165bdbf2a3869163a417f6944c288ab
Alan Young <harleypig@harleypig.com>
2024-03-19 16:26:00 -0600

    Refactored get_items script to use bw_serve_client for API requests.

--- examples/get_items.py
+++ examples/get_items.py
@@ -1,35 +1,3 @@
-import requests
-
-# Assuming the 'bw serve' command is running on localhost:8181 and is authenticated and unlocked
-def get_items():
-    # The URL to the 'list items' endpoint
-    url = "http://localhost:8181/list/object/items"
-
-    # Make a GET request to the server
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Return the JSON response if successful
-        return response.json()
-    else:
-        # Return None if the request failed
-        return None
-
-# The main function to execute when the script runs
-if __name__ == "__main__":
-    # Get the list of items from the vault
-    items = get_items()
-
-    # Check if we got a response
-    if items:
-        # Print the items if we did
-        print("Items retrieved from the vault:")
-        for item in items['data']:
-            print(item)
-    else:
-        # Print an error message if we did not
-        print("Failed to retrieve items from the vault.")
 from bw_serve_client.api_client import ApiClient
 from bw_serve_client.configuration import Configuration
 
@@ -39,6 +7,7 @@ config.host = "http://localhost:8181"
 
 # Create an instance of the API client
 client = ApiClient(configuration=config)
+
 
 def get_items():
     # The endpoint to list items
@@ -58,6 +27,7 @@ def get_items():
     else:
         # Return None if the request failed
         return None
+
 
 # The main function to execute when the script runs
 if __name__ == "__main__":
